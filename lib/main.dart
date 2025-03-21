c04f03b5681ad1c9e5991570d68fded48cd3ecad
LynokViktor <lynokviktor@gmail.com>
2025-03-21 23:52:15 +0100

    added search pannel

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:able_app/features/maps/presentation/blocs/location_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:able_app/features/maps/presentation/screens/main_map_screen.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'config/theme/app_theme.dart';
 
-void main() {
+void main() async{
   final bindings = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: bindings);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const AbleApp());
+  
 }
 
 class AbleApp extends StatelessWidget {
